9ad901fcff19de9dfe16b95c8eac1c322669ab92
Peter Kovacs <peter@sanebox.com>
2016-12-09 12:23:26 -0500

    Day 9: Define `adding` instead of building array of results and reducing.

--- Sources/Day9/main.swift
+++ Sources/Day9/main.swift
@@ -2,6 +2,6 @@ import Foundation
 import FootlessParser
 
 if let line = readLine(strippingNewline: true) {
-  print( try parse( zeroOrMore( decompress() ), line ).reduce( 0, + ) )
-  print( try parse( zeroOrMore( recursiveDecompress() ), line ).reduce( 0, + ) )
+  print( try parse( adding( decompress(), to: 0 ), line ) )
+  print( try parse( adding( recursiveDecompress(), to: 0 ), line ) )
 }
--- Sources/FootlessParser/Day9.swift
+++ Sources/FootlessParser/Day9.swift
@@ -1,5 +1,25 @@
 import Foundation
 
+public func adding<T,A: IntegerArithmetic>( _ x: Parser<T,A>, to start: A ) -> Parser<T,A> {
+  let p: Parser<T,A> = Parser { input in
+    var (first, remainder) = try x.parse(input)
+    var result = start + first
+
+    while true {
+      do {
+        let next = try x.parse( remainder )
+        result += next.output
+        remainder = next.remainder 
+      } catch {
+        return (result, remainder)
+      }
+    }
+  }
+
+  return optional(p, otherwise: start)
+}
+
+
 public func decompress() -> Parser<Character, Int> {
   // (#x#)
   let tagParser = 
@@ -36,7 +56,7 @@ public func recursiveDecompress() -> Parser<Character, Int> {
   let parser = tagParser <|> anyParser
 
   // our lazy recursive parser.
-  let recurse = lazy( oneOrMore( recursiveDecompress() ) <* eof() )
+  let recurse = lazy( adding( recursiveDecompress(), to: 0 ) <* eof() )
     
   return Parser { input in
     let (tag, remainder) = try parser.parse( input )
@@ -47,7 +67,7 @@ public func recursiveDecompress() -> Parser<Character, Int> {
 
     if remainder.startIndex < endIndex {
       let ( result, _ ) = try recurse.parse( remainder[ remainder.startIndex..<endIndex ] )
-      return ( tag.1 * result.reduce( 0, + ), remainder.dropFirst(tag.0) )
+      return ( tag.1 * result, remainder.dropFirst(tag.0) )
     } else {
       return ( tag.1, remainder )
     }
