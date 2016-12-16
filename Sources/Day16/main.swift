de37bb0d31f83864c8709801cf904f3087efc560
Peter Kovacs <peter@sanebox.com>
2016-12-16 14:08:57 -0500

    Day 16: Implement a mathematical solution rather than a generative one.
    Further optimizations are necessary.

--- Sources/Day16/main.swift
+++ Sources/Day16/main.swift
@@ -26,31 +26,71 @@ import FootlessParser
 // 
 // There definitely does seem to be a pattern to the data, but I can't find anything that actually repeats.
 
+// From the wikipedia page on Dragon Curves:
+// This pattern also gives a method for determining the direction of the nth
+// turn in the turn sequence of a Heighway dragon iteration. First, express n
+// in the form k2m where k is an odd number. The direction of the nth turn is
+// determined by k mod 4 i.e. the remainder left when k is divided by 4. If k
+// mod 4 is 1 then the nth turn is R; if k mod 4 is 3 then the nth turn is L.
 
-func dragon( data: [UInt8] ) -> [UInt8] {
-  var result = data
-  result.reserveCapacity( data.count * 2 + 1 )
-  result.append( 0 )
-  result.append( contentsOf: data.reversed().map { ($0 ^ 1) as UInt8 } )
-  return result
-}
+// There is a simple one line non-recursive method of implementing the above k
+// mod 4 method of finding the turn direction in code. Treating turn n as a
+// binary number, calculate the following boolean value:
 
-func checksum( data: ArraySlice<UInt8> ) -> [UInt8] {
-  var result = [UInt8]()
+// bool turn = (((n & −n) << 1) & n) != 0;
 
-  // Calculate largest power of 2 that divides data.count.
-  let chunksize = data.count & ~(data.count - 1)
-  result.reserveCapacity( chunksize )
+// Therefore we can easily calculate the nth bit using the `subscript` function
+// below.
 
-  for i in stride( from: 0, to: data.count, by: chunksize ) {
-    if data[ i..<(i+chunksize) ].reduce(0, { $0 + ($1 == 0 ? 1 : 0) }) % 2 == 0 {
-      result.append( 1 )
-    } else {
-      result.append( 0 )
-    }
+// Strangely, this method is actually slower (~ 400 msec) than directly
+// generating the data.
+
+struct Dragon {
+  let count: Int
+  let data: [[UInt8]]
+
+  init( data: [UInt8] ) {
+    self.count = data.count
+    self.data = [ data, data.reversed().map { ($0 ^ 1) as UInt8 } ]
   }
 
-  return result
+  func joiner( at: Int ) -> UInt8 {
+    let i = at + 1
+    return ((( i & -i ) << 1 ) & i) != 0 ? 1 as UInt8 : 0 as UInt8
+  }
+
+  func checksum( size: Int ) -> [UInt8] {
+    var result = [UInt8]()
+
+    // Calculate largest power of 2 that divides data.count.
+    let chunksize = size & ~(size - 1)
+    result.reserveCapacity( chunksize )
+
+    // OPTIMIZE: we know how many 1s are in data[0]+data[1] (its exactly data.count)
+    // So we could calculate the number of these groups that appear in the
+    // chunk + add in any joiners present.  Then we can increment by at least count + 1 each time.
+    for i in stride( from: 0, to: size, by: chunksize ) {
+      if (i..<(i+chunksize)).reduce(0, { $0 + (self[$1] == 0 ? 1 : 0) }) & 1 == 0 {
+        result.append( 1 )
+      } else {
+        result.append( 0 )
+      }
+    }
+
+    return result
+  }
+
+  subscript( i: Int ) -> UInt8 {
+    let index = i % ( count+1 )
+    let chunk = ( i / ( count+1 ) )
+
+    if index < count {
+      let which = chunk & 1
+      return data[which][index]
+    } else {
+      return joiner( at: chunk )
+    }
+  }
 }
 
 let zero = { _ in 0 as UInt8 } <^> char("0")
@@ -59,8 +99,5 @@ let parser = oneOrMore( zero <|> one )
 var data = try parse( parser, CommandLine.arguments[1] )
 let length = Int( CommandLine.arguments[2] )!
 
-while( data.count < length ) {
-  data = dragon( data: data )
-}
-
-print( checksum( data: data[ 0..<length ] ).map { "\($0)" }.joined() )
+let dragon = Dragon( data: data )
+print( dragon.checksum( size: length ).map{ "\($0)" }.joined() )
