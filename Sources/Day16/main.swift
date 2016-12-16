09730cc00eb6d91a551595ca967a7c6b9f8cb82f
Peter Kovacs <peter@sanebox.com>
2016-12-16 07:51:38 -0500

    Day 16 Checksum optimization.
    unchecked run time is down to ~ 0.2 seconds.

--- Sources/Day16/main.swift
+++ Sources/Day16/main.swift
@@ -13,6 +13,20 @@ import FootlessParser
 // repeat until end up with *odd* number of checksum bits.
 // 
 
+// it seems like we could figure out what the nth bit is in our final data set
+// by figuring out which multiple of our original data size + 1 we are in.
+//
+// If we're the last bit, then we need to calculate the "joiner" value.
+// If we're not, then we just figure out if we're in our original data or its
+// reversed/negated counterpart. And you can do that just by checking if the
+// dividend is even or not.
+// 
+// I'm not sure how to calculate the joiner, but its easy enough to generate -- you can do it with the same method.
+// But you still end up with DiskSize / OriginalInput.count+1 elements in your array.
+// 
+// There definitely does seem to be a pattern to the data, but I can't find anything that actually repeats.
+
+
 func dragon( data: [UInt8] ) -> [UInt8] {
   var result = data
   result.reserveCapacity( data.count * 2 + 1 )
@@ -23,22 +37,20 @@ func dragon( data: [UInt8] ) -> [UInt8] {
 
 func checksum( data: ArraySlice<UInt8> ) -> [UInt8] {
   var result = [UInt8]()
-  result.reserveCapacity( data.count / 2 + 1 )
 
-  for i in stride( from: 0, to: data.count, by: 2 ) {
-    switch ( data[i], data[ i + 1 ] ) {
-    case (0,0), (1, 1):
+  // Calculate largest power of 2 that divides data.count.
+  let chunksize = data.count & ~(data.count - 1)
+  result.reserveCapacity( chunksize )
+
+  for i in stride( from: 0, to: data.count, by: chunksize ) {
+    if data[ i..<(i+chunksize) ].reduce(0, { $0 + ($1 == 0 ? 1 : 0) }) % 2 == 0 {
       result.append( 1 )
-    default:
+    } else {
       result.append( 0 )
     }
   }
 
-  if result.count % 2 == 0 {
-    return checksum( data: ArraySlice<UInt8>(result) )
-  } else {
-    return result
-  }
+  return result
 }
 
 let zero = { _ in 0 as UInt8 } <^> char("0")
