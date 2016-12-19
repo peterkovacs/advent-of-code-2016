73bc8da391ecb0b7e65d2061205427711bc6ff55
Peter Kovacs <peter@sanebox.com>
2016-12-19 17:26:03 -0500

    Day 19: implement also using log2 and log3.

--- Sources/Day19/main.swift
+++ Sources/Day19/main.swift
@@ -6,18 +6,12 @@ import Foundation
 // 3005290 = 2^(21) + 908138
 // The answer is then 2l + 1
 
-func pow( _ base: Int, _ exponent: Int ) -> Int {
-  var result = 1
-  for _ in 1...exponent {
-    result = result &* base
-  }
-
-  return result
-}
-
 let input = Int( CommandLine.arguments[1] )!
 let part1: Int? = stride( from: 63, through: 1, by: -1 ).first() { input / (1 << $0) == 1 }.map { (input % (1 << $0) ) * 2 + 1 }
 print( "PART 1: \(part1!)" )
+
+let part1a = ((input & ~(1 << Int(log2( Double(input) )))) << 1) + 1
+print( "PART 1: \(part1a)" )
 
 // 
 // Part 2, each elf takes directly across.
@@ -27,9 +21,16 @@ print( "PART 1: \(part1!)" )
 // If we're in the first half, then the numbers simply go up normally.
 // If we're in the second half, we go up 2n + 1.
 
-let part2: Int? = stride( from: 1, through: 63, by: 1 ).first() { input <= pow(3, $0 + 1) }.map { 
-  let power = pow( 3, $0 )
+let part2: Int? = stride( from: 1, through: 63, by: 1 ).first() { input <= Int(pow(3.0, $0 + 1)) }.map { 
+  let power = Int(pow( 3.0, $0 ))
   return input - power + max( input - 2 * power, 0 )
 }
 
 print( "PART 2: \(part2!)" )
+
+let part2a = { () -> Int in
+  let power = Int(pow( 3.0, Double(Int( log( Double(input) )/log(3.0) ) )))
+  return input - power + max( input - 2 * power, 0 )
+}()
+
+print( "PART 2: \(part2a)" )
