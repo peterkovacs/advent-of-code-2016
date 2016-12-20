d9b40f15a8ed6e1d6767e0230d4231759aa4e2e8
Peter Kovacs <peter@sanebox.com>
2016-12-20 08:23:46 -0500

    Day 20: Rewrite using `sequence` function.

--- Sources/Day20/main.swift
+++ Sources/Day20/main.swift
@@ -5,22 +5,29 @@ import Lib
 let number = { Int($0)! } <^> oneOrMore( digit )
 let range = tuple <^> number <*> (char("-") *> number)
 
-let blacklist: [(Int,Int)] = {
-  // Parse the list
-  // Sort it by the low value in the tuple
-  // Reduce it to eliminate overlaps.
-  let list = Input().map { (line: String) -> (Int, Int) in try! parse( range, line ) }.sorted { $0.0 < $1.0 }.reduce( ([(Int,Int)](), (0,0)) ) {
-    if $0.1.1 + 1 >= $1.0 {
-      return ($0.0, ($0.1.0, max($0.1.1, $1.1)))
+let list = Input().map { (line: String) -> (Int, Int) in try! parse( range, line ) }.sorted { $0.0 < $1.0 }
+
+
+typealias State = ((Int,Int)?,Array<(Int,Int)>.Iterator)
+let test = sequence( state: ((0,0), list.makeIterator()) ) { (state: inout State) -> (Int,Int)? in
+  while true {
+    guard let prev = state.0 else { return nil }
+    guard let next = state.1.next() else { defer { state.0 = nil }; return state.0 }
+
+    if prev.1 + 1 >= next.0 {
+      state.0 = ( prev.0, max( next.1, prev.1 ) )
     } else {
-      return ( $0.0 + [ $0.1 ], $1 )
+      defer { state.0 = next }
+      return state.0
     }
   }
-  return list.0 + [ list.1 ]
-}()
+}
+
+
+let blacklist = Array(test)
 
 let lowest = blacklist.reduce(0) { $0 < $1.0 || $0 > $1.1 ? $0 : $1.1 + 1 }
 print( "PART 1: \(lowest)" )
 
-let part2 = blacklist.reduce( Int(UInt32.max) ) { $0 - ($1.1 - $1.0 + 1) } + 1
-print( "PART 2: \(part2)" )
+let available = blacklist.reduce( Int(UInt32.max) ) { $0 - ($1.1 - $1.0 + 1) } + 1
+print( "PART 2: \(available)" )
